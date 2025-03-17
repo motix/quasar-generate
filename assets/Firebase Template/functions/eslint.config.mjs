af479ff42b9a519c06c331cf06352ae7cf336176
Minh Nguyen <minh.nguyen@motiteam.com>
2025-03-17 14:24:23 +0700

    create-firebase

---
+++ assets/Firebase Template/functions/eslint.config.mjs
@@ -0,0 +1,41 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+import js from '@eslint/js';
+
+tseslint.configs.recommendedTypeChecked.forEach((config) => (config.files = ['**/*.ts']));
+
+export default [
+  {
+    ignores: ['lib/**/*', 'generated/**/*'],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true, allowTernary: true },
+      ],
+    },
+  },
+
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      globals: {
+        ...globals.node,
+      },
+
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.dev.json'],
+      },
+    },
+  },
+];
