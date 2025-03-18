a3e0042b1165b0d6c50831dcc251f749ffec7be2
Minh Nguyen <minh.nguyen@motiteam.com>
2025-03-18 20:31:28 +0700

    create-firebase

--- dist/create-firebase.js
+++ dist/create-firebase.js
@@ -197,7 +197,7 @@ setGlobalOptions({
   region: '${config.functionsRegion}',
 });
 
-// Making sure \`group.js\` only starts loading after \`setGlobalOptions\` is called
+// Making sure \`group.js\` only starts loading after \`setGlobalOptions\` was called
 const group = await import('./group.js');
 
 export const app = group;
