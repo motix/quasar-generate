02cf192d7ee40e1490afa78ef35bca8a952806ad
Quang Khánh <mnguyen284.ai@gmail.com>
2026-04-02 21:29:00 +0700

    Reset

--- dist/lib/commit-code.js
+++
@@ -1,6 +0,0 @@
-import { execSync } from 'child_process';
-export default function commitCode(projectFolder, message) {
-    execSync(`cd ${projectFolder.replaceAll(' ', '\\ ')} && git add . && git commit -q -m "${message}"`, {
-        stdio: 'inherit',
-    });
-}
