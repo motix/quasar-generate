cf7f0d67748ff5739d35a6f0213a52d6f86edf71
Quang Khánh <mnguyen284.ai@gmail.com>
2026-04-18 00:57:29 +0700

    feat: add npm publish workflow and bump project version to 2.1.10

--- projects/mnapp/project.js
+++ projects/mnapp/project.js
@@ -3,7 +3,7 @@ export default {
   projectFolder: '../../../GitHub/motix/quasar-app-extension-mnapp',
   organizationName: 'motinet',
   extensionId: 'mnapp',
-  version: '2.0.0',
+  version: '2.1.10',
   projectDescription: 'MotiNet Quasar Starter Kit as an App Extension',
   author: 'Minh Nguyen <minh.nguyen@motiteam.com>',
 };
