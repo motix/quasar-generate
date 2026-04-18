63456b301d4e37425e18069d4dde41d4cdf14e3b
Quang Khánh <mnguyen284.ai@gmail.com>
2026-04-18 18:50:58 +0700

    chore: remove .qgrc.js and add it to .gitignore

--- .qgrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-  assets: './assets',
-  projects: '../quasar-generate-projects',
-  output: '../quasar-generate-output',
-  commitCodeEnabled: true,
-  runYarn: false,
-  autoLaunch: false,
-};
